6f7e0045498bb3aa828d968b06acc9f216db6075
MartiRF <martinreyesrf@gmail.com>
2023-12-04 17:44:52 -0600

    HeroPage.tsx: add

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import HeroPage from './components/HeroPage';
 import Nav from './components/Nav';
 export default function Home() {
   return (
     <main className="h-full w-full ">
       <Nav />
+      <HeroPage />
     </main>
   )
 }
